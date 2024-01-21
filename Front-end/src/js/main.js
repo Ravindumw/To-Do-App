ba51875ad6d954571d03360d545bbce37cb24484
Ravindu <ravindumadusanka998@gmail.com>
2024-01-22 00:40:19 +0530

    Consume Load, Delete Task Web Services

--- Front-end/src/js/main.js
+++ Front-end/src/js/main.js
@@ -8,23 +8,28 @@ function loadAllTasks(){
     // Todo : load all tasks from the back-end
     // if success
 
-    const taskList = [
-        {id: 1, task: 'Task1', status: false},
-        {id: 2, task: 'Task2', status: true},
-        {id: 3, task: 'Task3', status: true},
-        {id: 4, task: 'Task4', status: false},
-    ];
-    taskList.forEach(task => createTask(task));
+    fetch('http://localhost:8080/tasks').then(res => {
+        if (res.ok){
+            res.json().then(taskList => taskList.forEach(task => createTask(task))) ;
+        }else{
+            alert("Failed to load task list");
+        }
+    }).catch(err => {
+        alert("Something went wrong, try again later");
+    });
 }
 
 function createTask(task){
 
     const newLiElm = document.createElement('li');
     taskContainerElm.append(newLiElm);
-
-    newLiElm.innerHTML = ` <input id="ck-task-${task.id}" type="checkbox" ${task.status? "checked" : ""}>
-    <label for="ck-task-${task.id}">${task.task}</label>
-    <i class="delete bi bi-trash"></i>` ;
+    newLiElm.id = "task-" + task.id;
+    
+    newLiElm.innerHTML = `
+        <input id="chk-task-${task.id}" type="checkbox" ${task.status ? "checked": ""}>
+        <label for="chk-task-${task.id}">${task.description}</label>
+        <i class="delete bi bi-trash"></i>    
+    `;
 
 }
 
@@ -34,7 +39,20 @@ taskContainerElm.addEventListener('click',(e)=>{
     if(e.target?.classList.contains('delete')){
         // todo : delete the task in back-end
         // if success
-        e.target.closest('li').remove();
+
+        const taskId = e.target.closest('li').id.subString(5);
+
+        fetch(`http://localhost:8080/tasks/${taskId}`, {method : 'DELETE'})
+        .then(res=>{
+            if(res.ok){
+                e.target.closest("li").remove();
+            }else{
+                alert("Failed to delete the task");
+            }
+        }).catch(err => {
+            alert("Something went wrong, try again later");
+        });
+        
     }else if(e.target?.tagName === "INPUT" ) {
         // Todo : update the task status in the back-end
         // If not success

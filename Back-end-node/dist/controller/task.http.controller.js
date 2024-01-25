4cb402f0cb0d7718529da513274c1971040ce172
Ravindu <ravindumadusanka998@gmail.com>
2024-01-25 23:38:17 +0530

    add connection pool, database and function

--- Back-end-node/dist/controller/task.http.controller.js
+++ Back-end-node/dist/controller/task.http.controller.js
@@ -1,20 +1,69 @@
 import { Router } from "express";
+import mysql from "mysql2/promise";
 const controller = Router();
 controller.get('/', getAllTasks);
 controller.post('/', saveTask);
 controller.patch('/:id', updateTask);
 controller.delete('/:id', deleteTask);
-function getAllTasks(req, res) {
-    res.send("<h1>Task Controller: Get</h1>");
-}
-function saveTask(req, res) {
-    res.send("<h1>Task Controller: Post</h1>");
-}
-function updateTask(req, res) {
-    res.send("<h1>Task Controller: Patch</h1>");
-}
-function deleteTask(req, res) {
-    res.send("<h1>Task Controller: Delete</h1>");
-}
 export { controller as TaskHttpController };
+const pool = mysql.createPool({
+    database: 'project_todo_app',
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'Ravindu123',
+    connectionLimit: 10
+});
+async function getAllTasks(req, res) {
+    if (!req.query.email)
+        res.sendStatus(400);
+    const connection = await pool.getConnection();
+    const [taskList] = await connection.execute('SELECT * FROM task WHERE email = ?', [req.query.email]);
+    res.json(taskList);
+    pool.releaseConnection(connection);
+}
+async function saveTask(req, res) {
+    const task = req.body;
+    const connection = await pool.getConnection();
+    const [{ insertId }] = await connection
+        .execute('INSERT INTO task (description, status, email) VALUES (?, false,?)', [task.description, task.email]);
+    pool.releaseConnection(connection);
+    task.id = insertId;
+    task.status = false;
+    res.status(201).json(task);
+}
+async function updateTask(req, res) {
+    const taskId = +req.params.id;
+    const task = req.body;
+    const connection = await pool.getConnection();
+    const [result] = await connection.
+        execute('SELECT * FROM task WHERE id= ?', [taskId]);
+    if (!result.length) {
+        res.sendStatus(404);
+        return;
+    }
+    else {
+        await connection.
+            execute('UPDATE task SET description = ?, status = ? WHERE id = ?', [task.description, task.status, taskId]);
+        res.sendStatus(204);
+    }
+    pool.releaseConnection(connection);
+}
+async function deleteTask(req, res) {
+    const taskId = +req.params.id;
+    const task = req.body;
+    const connection = await pool.getConnection();
+    const [result] = await connection.
+        execute('SELECT * FROM task WHERE id= ?', [taskId]);
+    if (!result.length) {
+        res.sendStatus(404);
+        return;
+    }
+    else {
+        await connection.
+            execute('DELETE FROM task WHERE id = ?', [taskId]);
+        res.sendStatus(204);
+    }
+    pool.releaseConnection(connection);
+}
 //# sourceMappingURL=task.http.controller.js.map
--- Back-end-node/src/controller/task.http.controller.ts
+++ Back-end-node/src/controller/task.http.controller.ts
@@ -1,5 +1,7 @@
 import {Router} from "express";
 import {Request, Response} from "express";
+import mysql, {ResultSetHeader, RowDataPacket} from "mysql2/promise";
+import {TaskTO} from "../to/TaskTO.js";
 
 const controller = Router();
 
@@ -7,19 +9,68 @@ controller.get('/',getAllTasks);
 controller.post('/',saveTask);
 controller.patch('/:id',updateTask);
 controller.delete('/:id',deleteTask);
-
-function getAllTasks(req: Request,res: Response){
-    res.send("<h1>Task Controller: Get</h1>");
-}
-function saveTask(req: Request,res: Response){
-    res.send("<h1>Task Controller: Post</h1>");
-}
-function updateTask(req: Request,res: Response){
-    res.send("<h1>Task Controller: Patch</h1>");
-}
-function deleteTask(req: Request,res: Response){
-    res.send("<h1>Task Controller: Delete</h1>");
-}
-
 export {controller as TaskHttpController};
+
+const pool = mysql.createPool({
+        database: 'project_todo_app',
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'Ravindu123',
+        connectionLimit: 10
+});
+
+async function getAllTasks(req: Request,res: Response){
+    if (!req.query.email) res.sendStatus(400);
+    const connection = await pool.getConnection();
+    const [taskList] = await connection.execute('SELECT * FROM task WHERE email = ?', [req.query.email]);
+    res.json(taskList);
+    pool.releaseConnection(connection);
+}
+async function saveTask(req: Request,res: Response){
+    const task = <TaskTO> req.body;
+    const connection = await pool.getConnection();
+    const [{insertId}] = await connection
+        .execute<ResultSetHeader>('INSERT INTO task (description, status, email) VALUES (?, false,?)',
+        [task.description,task.email]);
+    pool.releaseConnection(connection);
+    task.id = insertId;
+    task.status = false;
+    res.status(201).json(task);
+}
+async function updateTask(req: Request,res: Response){
+    const taskId = +req.params.id;
+    const task = <TaskTO> req.body;
+    const connection = await pool.getConnection();
+    const [result] = await connection.
+    execute<RowDataPacket[]>('SELECT * FROM task WHERE id= ?', [taskId]);
+    if (!result.length){
+        res.sendStatus(404);
+        return;
+    }else {
+        await connection.
+        execute<RowDataPacket[]>('UPDATE task SET description = ?, status = ? WHERE id = ?',
+            [task.description, task.status, taskId]);
+        res.sendStatus(204);
+    }
+    pool.releaseConnection(connection);
+}
+async function deleteTask(req: Request,res: Response){
+    const taskId = +req.params.id;
+    const task = <TaskTO> req.body;
+    const connection = await pool.getConnection();
+    const [result] = await connection.
+    execute<RowDataPacket[]>('SELECT * FROM task WHERE id= ?', [taskId]);
+    if (!result.length){
+        res.sendStatus(404);
+        return;
+    }else {
+        await connection.
+        execute<RowDataPacket[]>('DELETE FROM task WHERE id = ?',
+            [taskId]);
+        res.sendStatus(204);
+    }
+    pool.releaseConnection(connection);
+}
+
 
